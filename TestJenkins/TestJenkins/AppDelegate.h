0b382dfdeeca010ecd3a12226fa236dc14b5c6ea
YotrolZ <674317983@qq.com>
2016-10-09 10:26:00 +0800

    初始化项目

---
+++ TestJenkins/TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by Sunshine on 2016/10/9.
+//  Copyright © 2016年 Sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestJenkins/TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by Sunshine on 2016/10/9.
+//  Copyright © 2016年 Sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
